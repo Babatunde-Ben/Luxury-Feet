0de83bc133fc94f1054274a7bc4a512b57383925
Babatunde-Ben <babatundeben110@gmail.com>
2022-03-28 14:03:20 +0100

    modified hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#ffffff",
+        secondary: {
+          100: "#f87171",
+          200: "#424242",
+        },
+      },
       fontFamily: {
         Poppins: ["Poppins", "sans-serif"],
       },
