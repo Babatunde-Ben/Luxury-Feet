729c5fdeeed6e1b3dbcd93d3165d3026aa79d054
Babatunde-Ben <babatundeben110@gmail.com>
2022-03-31 04:33:47 +0100

    added testimonials

--- public/script.js
+++ public/script.js
@@ -1,9 +1,11 @@
-console.log(`script is working fine`);
-const menuIcon = document.getElementById("menu-icon");
+const menuIconBtn = document.getElementById("menu-icon");
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".close-menu");
+console.log(openMenu);
+console.log(closeMenu);
 const navBar = document.querySelector(".top-navbar");
-console.log(menuIcon);
-console.log(navBar);
-menuIcon.addEventListener("click", () => {
+menuIconBtn.addEventListener("click", () => {
   navBar.classList.toggle("hidden");
-  console.log(`menu icon is clicked`);
+  openMenu.classList.toggle("hidden");
+  closeMenu.classList.toggle("hidden");
 });
