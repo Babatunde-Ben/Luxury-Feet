1e9948067ecf43e013e6f4fa6d8c1e7207161df8
Babatunde-Ben <babatundeben110@gmail.com>
2022-04-02 16:05:42 +0100

    modified font

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
           100: "#f87171",
           200: "#424242",
         },
+        facebook: "#3A5795",
+        instagram: "#E84832",
+        twitter: "#329CEA",
+        whatsapp: "#43BF53",
       },
       fontFamily: {
         Poppins: ["Poppins", "sans-serif"],
