1926dba6f5e43971f5a035a2d17835f1a47f77cd
Babatunde-Ben <babatundeben110@gmail.com>
2022-04-04 03:21:18 +0100

    modified section paddings

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
 const menuIconBtn = document.getElementById("menu-icon");
 const openMenu = document.querySelector(".open-menu");
 const closeMenu = document.querySelector(".close-menu");
-console.log(openMenu);
-console.log(closeMenu);
 const navBar = document.querySelector(".top-navbar");
+
 menuIconBtn.addEventListener("click", () => {
-  navBar.classList.toggle("hidden");
+  if (navBar.classList.contains("h-0")) {
+    navBar.classList.remove("h-0");
+    navBar.classList.add("h-auto");
+  } else {
+    navBar.classList.add("h-0");
+    navBar.classList.remove("h-auto");
+  }
+  // navBar.classList.toggle("overflow-auto");
+
   openMenu.classList.toggle("hidden");
   closeMenu.classList.toggle("hidden");
 });
